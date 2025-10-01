eb96b685493aac6ab85ddbc3ba38ccf17275b284
lukezheng2011 <lukezheng2011@gmail.com>
2025-10-01 09:33:37 -0400

    numguess
    WILLIAM LI IS A GOOD BOY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- numberguess.py
+++ numberguess.py
@@ -1,18 +1,25 @@
 import random
 
+randomnum = random.randint(1,10)
 
 
 
+guessed = 0
+guesslist = []
 
+while guessed == 0:
+    inpoot = input("Enter a number: ")
 
+    if inpoot > randomnum:
+        guesslist.append(f"")
+        print("The number is LESS than your input")
 
+    elif inpoot < randomnum:
+        guesslist.append(inpoot)
+        print("The number is MORE than your input")
 
-
-
-
-
-
-
+    else:
+        print(f"You have found the number. It was {randomnum}")
 
 
 
