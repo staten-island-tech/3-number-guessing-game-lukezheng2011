a9fafd2fb62905435a2b8b999884c942062cf14c
lukezheng2011 <lukezheng2011@gmail.com>
2025-10-03 09:12:30 -0400

    a
    a

--- numberguess.py
+++ numberguess.py
@@ -1,6 +1,27 @@
 import random
 
-randomnum = random.randint(1,10)
+###randomnum = random.randint(1,100)
+
+randomnum = 1241241421
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -8,19 +29,21 @@ guessed = 0
 guesslist = []
 
 while guessed == 0:
-    inpoot = input("Enter a number: ")
+    inpoot = int(input("Enter a number: "))
 
     if inpoot > randomnum:
-        guesslist.append(f"")
+        guesslist.append(inpoot)
         print("The number is LESS than your input")
+        print(f"You've guessed {guesslist}")
 
     elif inpoot < randomnum:
         guesslist.append(inpoot)
         print("The number is MORE than your input")
+        print(f"You've guessed {guesslist}")
 
     else:
-        print(f"You have found the number. It was {randomnum}")
-
+        print(f"You have found the number. It was {randomnum} Your guesses were {guesslist}")
+        guessed = 1
 
 
 
