eb58f28f8e7458c47ee0a3ae83e31ae8f6821f2b
lukezheng2011 <lukezheng2011@gmail.com>
2025-10-06 09:24:39 -0400

    aaaa
    aaa

---
+++ engorfrench.py
@@ -0,0 +1,20 @@
+
+
+
+def lang(x):
+    numberS = 0
+    numberT = 0
+    for char in x:
+        if char == "S" or char == "s":
+            numberS = numberS+1
+        if char == "T" or char == "t":
+            numberT=numberT+1
+
+    if numberT > numberS:
+        print("Your input is likely English.")
+    elif numberS >= numberT:
+        print("Your text is likely French.")
+
+
+
+lang(input("Enter your text: "))
